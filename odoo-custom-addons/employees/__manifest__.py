11ba6e7ba975ca6a0fc12146abfe2f764ef0a49a
iticGuifre <2023_guifre.salvat@iticbcn.cat>
2024-05-19 19:06:31 +0200

    Primera versio de empleats

---
+++ odoo-custom-addons/employees/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employees_Hospital",
+
+    'sequence': -100,
+
+    'summary': """
+        TIC Hospital Employees Management""",
+
+    'description': """
+        Aplicacion para gestionar los empleados del hospital TIC
+    """,
+
+    'author': "Guifre Company",
+    'website': "http://www.tichospital.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Health',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
